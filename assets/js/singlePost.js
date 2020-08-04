704f8cf58dba89eef8b8d5cfbd7ca9085a42a352
dhafagk <dhafaggtq@gmail.com>
2020-08-04 23:27:58 +0700

    hai

---
+++ assets/js/singlePost.js
@@ -0,0 +1,76 @@
+/*=== SEARCH FILTER ====*/
+/*    https://codepen.io/alexerlandsson/pen/ZbyRoO */
+$('[data-search]').on('keyup', function() {
+	var searchVal = $(this).val();
+	var filterItems = $('[data-filter-item]');
+
+	if ( searchVal != '' ) {
+		filterItems.addClass('collapse');
+		$('[data-filter-item][data-filter-name*="' + searchVal.toLowerCase() + '"]').removeClass('collapse');
+	} else {
+		filterItems.removeClass('collapse');
+	}
+});
+/*=== SCROLL SMUT ====*/
+$(document).ready(function(){
+    if(location.hash != null && location.hash != ""){
+        $(location.hash + '.collapse').collapse('show');
+    }
+    
+    if(window.location.hash) {
+      var hash = window.location.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 500)
+    };
+    
+    $('.mirror').click(function(){
+                $('.mirror').removeClass("active");
+                $(this).addClass("active");
+            });
+});
+
+/*=== Logika Iframe ====*/
+$(document).ready(function(){
+    /*DataBase*/
+    var database = [
+        "https://www.youtube.com/embed/joV58f4IIV0", 
+        "https://www.youtube.com/embed/9XOY8bHkcYM", 
+        "https://www.youtube.com/embed/vbdTNtoMpn0"];
+    /*Mari Buat Wadah Default*/
+    var iframeID = '';
+    /*KONDISI SAAT DI CLICK*/
+    $('a.targetClick').click(function() {
+        /*Mengambil attribute dari A*/
+        iframeID = $(this).attr('data-id');
+        iframeID = $(this).attr('data-id');
+        
+        /*Change Attribute Iframe*/
+        $('iframe.target').hide();
+        $('iframe.target[data-id='+iframeID+']').show();
+        
+        $('iframe.target').prop('src', "Null");
+        $('iframe.target[data-id='+iframeID+']').prop('src', database[iframeID]+'?autoplay=1');
+        
+    });/*a.targetClick*/
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
